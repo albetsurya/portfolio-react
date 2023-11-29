eb03f21ced0cc24d8c80ace24c318264f7e09cf6
= <=>
2023-11-29 13:22:58 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+        <div className="footer-nav">
+          <div size={12} sm={6}>
             <div className="brand-logo">
               <img src={logo} alt="Logo" />
             </div>
-          </Col>
+          </div>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon ">
               <a href="https://linkedin.com/in/albetsurya/">
@@ -31,7 +31,7 @@ export const Footer = () => {
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
-        </Row>
+        </div>
       </Container>
     </footer>
   );
